24655940d2087a43b392dd603f594f9130471515
Tony <buddy90_lost@yahoo.co.id>
2017-10-04 02:13:45 +0700

    Update AlGeo.java

--- Semester 3/AlGeo/AlGeo.java
+++ Semester 3/AlGeo/AlGeo.java
@@ -4,18 +4,44 @@ import java.util.*;
 
 class AugmentedMatrix{
 	public double mat[][];
-	public int row;
-	public int col;
+	public int rows;
+	public int cols;
+	public boolean Infinit;
 	public AugmentedMatrix(int rows, int cols){
-		this.mat = new double[rows][cols+1];
-		this.row = rows;
-		this.col = cols;
+		this.mat = new double[cols][cols+1];
+		this.rows = rows;
+		this.cols = cols;
+		this.Infinit = false;
+	}
+	public AugmentedMatrix(AugmentedMatrix t){
+		this.mat = t.mat;
+		this.rows = t.rows;
+		this.cols = t.cols;
+		this.Infinit = t.Infinit;
 	}
 	public double Elmt(int i, int j){
 		return this.mat[i][j];
 	}
 	public void SetElmt(int i, int j, double val){
 		this.mat[i][j] = val;
+	}
+	public boolean IsFull0(int i){
+		int j;
+		for(j=0;j<this.cols;j++){
+			if(Math.abs(this.Elmt(i,j)) > 0.0000000001)
+				return false;
+		}
+		return true;
+	}
+	public boolean IsSolutionExist(){
+		int i,j;
+		for(i=0;i<this.cols;i++){
+			if(this.IsFull0(i)){
+				if(Math.abs(this.Elmt(i,cols)) > 0.0000000001)
+					return false;
+			}
+		}
+		return true;
 	}
 }
 
@@ -42,76 +68,98 @@ class ColumnVector{
 	}
 }
 
+class Parameter{
+	public int[] var;
+	public double[] koef;
+	public double[] cons;
+	public int size;
+	//Konstruktor
+	public Parameter(int s){
+		this.var = new int[s];
+		this.koef = new double[s];
+		this.cons = new double[s];
+		this.size = s;
+	}
+	public int GetVar(int i){
+		return this.var[i];
+	}
+	public double GetKoef(int i){
+		return this.koef[i];
+	}
+	public double GetConst(int i){
+		return this.cons[i];
+	}
+	//Setter
+	public void SetVar(int i, int val){
+		this.var[i] = val;
+	}
+	public void SetKoef(int i, double val){
+		this.koef[i] = val;
+	}
+	public void SetConst(int i, double val){
+		this.cons[i] = val;
+	}
+}
+
 public class AlGeo{
-	public static boolean Infinit;
-	
-	public static AugmentedMatrix ReadInputMatrix(){
-		Scanner read = new Scanner(System.in);
-		int row, col;
-		System.out.print("Masukkan banyak persamaan : ");
-		row = read.nextInt();
-		System.out.print("Masukkan banyak variabel : ");
-		col = read.nextInt();
-		AugmentedMatrix mat = new AugmentedMatrix(row, col);
-		for(int i=0;i<row;i++){
-			for(int j=0;j<=col;j++){
-				mat.SetElmt(i,j,read.nextDouble());
-			}
-		}
-		for(int i=row; i<col;i++){
-			for(int j=0;j<=col;j++){
-				mat.SetElmt(i,j,0);
-			}
-		}
-		return mat;
+	public static Parameter ParameterSolve(AugmentedMatrix mat){
+		Parameter par = new Parameter(mat.cols);
+		
+		return par;
 	}
 	
-	public static ColumnVector GaussianElimination(AugmentedMatrix mat){
+	public static AugmentedMatrix GaussianElimination(AugmentedMatrix mat){
 		int i, j, k, l;
 		double t;
-		ColumnVector Ans = new ColumnVector(mat.col);
 		
-		for (i = 0; i < mat.row-1; i++) { // the forward elimination phase
+		for (i = 0; i < mat.rows-1; i++) { // the forward elimination phase
 			l = i;
-			for (j = i + 1; j < mat.col; j++) {// which row has largest column value
+			for (j = i + 1; j < mat.cols; j++) {// which row has largest column value
 				if (Math.abs(mat.Elmt(j,i)) > Math.abs(mat.Elmt(l,i))) {
 					l = j; // remember this row l
 				}
 			}
 			// swap this pivot row, reason: minimize floating point error
-			for (k = i; k <= mat.col; k++) {// t is a temporary double variable
+			for (k = i; k <= mat.cols; k++) {// t is a temporary double variable
 				t = mat.Elmt(i,k);
 				mat.SetElmt(i,k,mat.Elmt(l,k));
 				mat.SetElmt(l,k,t);
 			}
-			for (j = i + 1; j < mat.row; j++) {// the actual forward elimination phase
-				for (k = mat.col; k >= i; k--) {
+			for (j = i + 1; j < mat.rows; j++) {// the actual forward elimination phase
+				for (k = mat.cols; k >= i; k--) {
 					mat.SetElmt(j, k, mat.Elmt(j,k)-(mat.Elmt(i,k)*mat.Elmt(j,i)/mat.Elmt(i,i)));
 				}
 			}
 		}
 		
-		for(i = 0; i < mat.row; i++) {
-			Infinit = true;
-			for(j = 0; j < mat.col; j++) {
+		for(i = 0; i < mat.rows; i++) {
+			mat.Infinit = true;
+			for(j = 0; j < mat.cols; j++) {
 				if (Math.abs(mat.Elmt(i,j)) > 0.0000000001) {					
-					Infinit = false;
+					mat.Infinit = false;
 					break;
 				}
 			}
-			if (Infinit) {
+			if (mat.Infinit) {
 				break;
 			} 
 		}
 		
-		for (j = mat.row - 1; j >= 0; j--) { // start from back
-			for (t = 0.0, k = j + 1; k < mat.col; k++) {
+		return mat;	
+	}
+	
+	static ColumnVector GetSolution(AugmentedMatrix mat){
+		ColumnVector Ans = new ColumnVector(mat.cols);
+		int  j, k;
+		double t;
+		
+		for (j = mat.rows - 1; j >= 0; j--) { // start from back
+			for (t = 0.0, k = j + 1; k < mat.cols; k++) {
 				t += mat.Elmt(j,k) * Ans.Elmt(k);
 			}
-			Ans.SetElmt(j,(mat.Elmt(j,mat.col)-t)/mat.Elmt(j,j)); // the answer is here
+			Ans.SetElmt(j,(mat.Elmt(j,mat.cols)-t)/mat.Elmt(j,j)); // the answer is here
 		}
-
-		return Ans;	
+		return Ans;
 	}
 	
 	static void ReadFileNSolve(){
@@ -154,31 +202,73 @@ public class AlGeo{
 					}
 				}
 				//Solve it
-				vec = new ColumnVector(GaussianElimination(mat));
+				mat = new AugmentedMatrix(GaussianElimination(mat));
 				//Print to console and file
 				System.out.println("Input "+i);
 				fwrite.println("Input "+i);
-				if (Infinit) {
+				if(!mat.IsSolutionExist()){
+					System.out.println("No Solution Exist");
+				}
+				if (mat.Infinit) {
 					System.out.println("Infinit Solution");
 				}
-				for(j=0;j<vec.size;j++){
-					System.out.printf("var_%d = %.2f\n", j+1, vec.Elmt(j));
-					fwrite.printf("var_%d = %.2f\n", j+1, vec.Elmt(j));
+				else{
+					vec = new ColumnVector(GetSolution(mat));
+					for(j=0;j<vec.size;j++){
+						System.out.printf("var_%d = %.2f\n", j+1, vec.Elmt(j));
+						fwrite.printf("var_%d = %.2f\n", j+1, vec.Elmt(j));
+					}
 				}
 				System.out.println();
 				fwrite.println();
 			}
 			readf.close();
 			fwrite.close();
-			read.close();
 		}
 		catch(Exception e){ // if the file not exist
 			e.printStackTrace();
 		}
 	}
 	
+	public static void ReadInputMatrixNSolve(){
+		Scanner read = new Scanner(System.in);
+		int row, col, j, i;
+		System.out.print("Masukkan banyak persamaan : ");
+		row = read.nextInt();
+		System.out.print("Masukkan banyak variabel : ");
+		col = read.nextInt();
+		AugmentedMatrix mat = new AugmentedMatrix(row, col);
+		ColumnVector vec = new ColumnVector(row);
+		Parameter par = new Parameter(col);
+		for(i=0;i<row;i++){
+			for(j=0;j<=col;j++){
+				mat.SetElmt(i,j,read.nextDouble());
+			}
+		}
+		for(i=row; i<col;i++){
+			for(j=0;j<=col;j++){
+				mat.SetElmt(i,j,0);
+			}
+		}
+		//Solve
+		mat = new AugmentedMatrix(GaussianElimination(mat));
+		if(!mat.IsSolutionExist()){
+			System.out.println("No Solution Exist");
+		}
+		if (mat.Infinit) {
+			System.out.println("Infinit Solution");
+			
+		}
+		else{
+			vec = new ColumnVector(GetSolution(mat));
+			for(j=0;j<vec.size;j++){
+				System.out.printf("var_%d = %.2f\n", j+1, vec.Elmt(j));
+			}
+		}
+	}
+	
 	static void ShowMenu(){
-		System.out.println("Opsi pilihan");
+		System.out.println("Menu - Program penyelesai SPL");
 		System.out.println("0 - Info program dan credits");
 		System.out.println("1 - Input SPL dari console");
 		System.out.println("2 - Baca SPL dari file eksternal");
@@ -189,32 +279,37 @@ public class AlGeo{
 	
 	public static void main(String []args){
 		//AugmentedMatrix matrix = ReadInputMatrix();
-		Scanner read = new Scanner(System.in);
-		String pil;
+		Scanner r = new Scanner(System.in);
+		int pil;
 		ShowMenu();
-		pil = read.nextLine();
-		while(pil!="4"){
-			if(pil == "0"){
-				
+		pil = r.nextInt();
+		while(pil!=4){
+			if(pil == 0){
+				System.out.println("Program penyelesai sistem persamaan linier");
+				System.out.println("Dibuat oleh :");
+				System.out.println("1) Tony - 13516010");
+				System.out.println("2) Harry Setiawan Hamjaya - 13516079");
+				System.out.println("3) Muh Habibi Haidir - 13516085");
+				System.out.println("Prodi Teknik Informatika");
+				System.out.println("Sekolah Teknik Elektro dan Informatika");
+				System.out.println("Institut Teknologi Bandung");
 			}
-			else if(pil == "1"){
-				
+			else if(pil == 1){
+				ReadInputMatrixNSolve();
 			}
-			else if(pil == "2"){
-				
+			else if(pil == 2){
+				ReadFileNSolve();
 			}
-			else if(pil == "3"){
-				
-			}
-			else if(pil == "4"){
+			else if(pil == 3){
 				
 			}
 			else{
 				System.out.println("Masukkan Salah!!");
 			}
-			PrintMenu();
-			pil = read.nextLine();
+			System.out.println();
+			ShowMenu();
+			pil = r.nextInt();
 		}
-		ReadFileNSolve();
+		System.out.println("Sampai jumpa");
 	}
 };
