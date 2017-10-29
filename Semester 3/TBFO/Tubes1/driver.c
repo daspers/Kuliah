97f15ba6d43825fa74be57b58be11e866b6cc876
Tony <buddy90_lost@yahoo.co.id>
2017-10-29 13:13:38 +0700

    Create driver.c

---
+++ Semester 3/TBFO/Tubes1/driver.c
@@ -0,0 +1,24 @@
+#include "dfa.h"
+
+int main(){
+	/* Kamus */
+	ArrSTATUS TabS;
+	ArrALPHA TabA;
+	ArrINPUT TabI;
+	RELASI R;
+	int IdxStart;
+	/* Algoritma */
+	//Baca DFA dari file external dan disimpan ke variabel
+	BacaFile(&TabS, &TabA, &R, &IdxStart);
+	printf("kk tes\n");
+	//Tulis data-data DFA yang dibaca ke layar console/terminal
+	TulisData(TabS, TabA, R, IdxStart);
+	printf("kk tes\n");
+	//Baca Input DFA dari file external dan disimpan ke variabel
+	BacaInputDFA(TabA, R, &TabI, IdxStart);
+	printf("kk tes\n");
+	//Tulis hasil dari Input DFA ke file external
+	TulisHasil(TabS, TabA, TabI, R, IdxStart);
+	printf("kk tes\n");
+	return 0;
+}
